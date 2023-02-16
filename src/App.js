02fd395e6c9aa144d0a9c6bb36e1726653095211
Jamie Evans <115733254+WilliamJamieEvans@users.noreply.github.com>
2023-02-16 11:27:38 +0000

    basic nav bar created component NavigationBar and called to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './Components/NavigationBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <NavigationBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
