d48497d2d74c325586e68d47d52767a24015500b
Steve Ayres <115733238+Weevey@users.noreply.github.com>
2023-02-16 19:58:38 +0000

    Added Header component and moved nav bar into it

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 function NavigationBar() {
   return (
     <nav className="navigation-bar">
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/flights">Flights</a></li>
-        <li><a href="/weather">Weather</a></li>
-        <li><a href="/about">About</a></li>
-      </ul>
+      <a href="/">Home | </a>
+        <a href="/flights">Flights | </a>
+        <a href="/weather">Weather | </a>
+        <a href="/about">About</a>
     </nav>
   );
 }
